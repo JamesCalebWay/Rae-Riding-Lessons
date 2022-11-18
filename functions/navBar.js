3b75350bcbf7b6cf5e0de3f852c16a0c74d80e9c
JamesCalebWay <77420065+JamesCalebWay@users.noreply.github.com>
2022-11-18 15:54:38 -0500

    Update navBar.js

--- functions/navBar.js
+++ functions/navBar.js
@@ -1,3 +1,11 @@
+// Student Name: James Caleb Way
+// Program Name: Rae Riding Lessons
+// Creation Date: 9/17/2022
+// Last Modified Date: 11/9/2022
+// CSCI Course: CSCI 495
+// Grade Received: TBA
+// Design Comments: 
+
 // Navigation menu addapted from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 
 // Highlights the current page
